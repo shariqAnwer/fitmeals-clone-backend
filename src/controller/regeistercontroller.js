3ff67b421d0d27480686316d43464b5b022d2699
mbnaveen <navinsmb@gmail.com>
2022-05-07 16:59:34 +0530

    jj

--- src/controller/regeistercontroller.js
+++ src/controller/regeistercontroller.js
@@ -1,46 +1,46 @@
-const user = require("../model/regeistermodel.js");
-const express = require("express");
-var jwt = require('jsonwebtoken');
-const generatetoken = (user) => {
-    return jwt.sign({ user }, 'shhhhh');
-}
+// const user = require("../model/regeistermodel.js");
+// const express = require("express");
+// var jwt = require('jsonwebtoken');
+// const generatetoken = (user) => {
+//     return jwt.sign({ user }, 'shhhhh');
+// }
 
-const route = express.Router();
+// const route = express.Router();
 
 
-// route.get("", (req, res, next) => {
+// // route.get("", (req, res, next) => {
+// //     try {
+
+// //         console.log("x");
+// //         return res.send("x")
+// //     }
+// //     catch (error) {
+// //         return res.send({ error: error.message })
+// //     }
+// // })
+
+// route.post("", async (req, res, next) => {
 //     try {
 
-//         console.log("x");
-//         return res.send("x")
+//         let User = await user.find({ email: req.body.email }).lean().exec();
+//         console.log(User)
+//         if (User.length !== 0) {
+//             return res.send({ message: "user already exist" });
+//             console.log(User, "nnnnnn")
+//         }
+//         let x = await user.create(req.body);
+//         console.log(x)
+//         const token = generatetoken(x);
+
+
+//         return res.send({ x, token });
 //     }
 //     catch (error) {
 //         return res.send({ error: error.message })
 //     }
 // })
 
-route.post("", async (req, res, next) => {
-    try {
-
-        let User = await user.find({ email: req.body.email }).lean().exec();
-        console.log(User)
-        if (User.length !== 0) {
-            return res.send({ message: "user already exist" });
-            console.log(User, "nnnnnn")
-        }
-        let x = await user.create(req.body);
-        console.log(x)
-        const token = generatetoken(x);
-
-
-        return res.send({ x, token });
-    }
-    catch (error) {
-        return res.send({ error: error.message })
-    }
-})
 
 
 
-
-module.exports = route
+// module.exports = route
