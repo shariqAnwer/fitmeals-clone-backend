cc08d75f3b5f68d0b9e77ea03b726fe914dfe834
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-05-07 17:06:11 +0530

    env port

--- src/controller/logincontroller.js
+++ src/controller/logincontroller.js
@@ -1,38 +1,39 @@
-// const express = require("express");
-// const users = require("../model/regeistermodel");
-// const bcrypt = require('bcrypt');
+const express = require("express");
+const users = require("../model/regeistermodel");
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
-// const verifytoken = (token) => {
-//     return jwt.verify(token, 'shhhhh', function (err, decoded) {
-//         console.log(decoded.foo) // bar
-//     });
+const verifytoken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
+        console.log(decoded.foo) // bar
+    });
 
-// }
+}
 
-// const route = express.Router();
+const route = express.Router();
 
 
-// route.post("", async (req, res, next) => {
+route.post("", async (req, res, next) => {
 
-//     try {
+    try {
 
-//         let x = await users.findOne({ email: req.body.username }).lean().exec();
-//         if (x.length == 0) {
-//             return res.send(" please regeister first")
-//         }
-//         if (x) {
-//             console.log(x)
-//             let confirm = bcrypt.compareSync(req.body.password, x.password);
-//             if (confirm) {
-//                 return res.send("login success")
-//             }
+        let x = await users.findOne({ email: req.body.username }).lean().exec();
+        if (x.length == 0) {
+            return res.send(" please regeister first")
+        }
+        if (x) {
+            console.log(x)
+            let confirm = bcrypt.compareSync(req.body.password, x.password);
+            if (confirm) {
+                return res.send("login success")
+            }
 
-//         }
-//         return res.send("Check username or password")
-//     }
-//     catch (error) {
-//         return res.send(error.message)
-//     }
-// })
+        }
+        return res.send("Check username or password")
+    }
+    catch (error) {
+        return res.send(error.message)
+    }
+})
 
-// module.exports = route
+module.exports = route
