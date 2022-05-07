880b0d1ae3d015643e0c6a7e060ed30b756a0916
mbnaveen <navinsmb@gmail.com>
2022-05-07 16:24:28 +0530

    back

--- src/model/regeistermodel.js
+++ src/model/regeistermodel.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
+// const mongoose = require("mongoose");
+// const bcrypt = require('bcrypt');
 
 
-const userschema = mongoose.Schema({
+// const userschema = mongoose.Schema({
 
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    mbl_number: { type: Number, required: true },
-    email: { type: String, required: true },
-
-
-})
-userschema.pre("save", function (next) {
-    const hash = bcrypt.hashSync(this.password, 8);
-    this.password = hash;
-    return next();
-});
+//     name: { type: String, required: true },
+//     password: { type: String, required: true },
+//     mbl_number: { type: Number, required: true },
+//     email: { type: String, required: true },
 
 
+// })
+// userschema.pre("save", function (next) {
+//     const hash = bcrypt.hashSync(this.password, 8);
+//     this.password = hash;
+//     return next();
+// });
 
 
-const users = mongoose.model("user", userschema);
 
-module.exports = users;
+
+// const users = mongoose.model("user", userschema);
+
+// module.exports = users;
 
