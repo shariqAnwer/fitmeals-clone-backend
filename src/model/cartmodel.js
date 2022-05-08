2b3404550e32c2fcd69bd56ace3991e68e48416a
mbnaveen <navinsmb@gmail.com>
2022-05-08 19:55:25 +0530

    commit

--- src/model/cartmodel.js
+++ src/model/cartmodel.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const cartschema = new mongoose.Schema({
 
-    image: { type: String, required: true },
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
+    image: { type: String, required: false },
+    title: { type: String, required: false },
+    price: { type: Number, required: false },
+    description: { type: String, required: false },
     about_product: [{ type: String, required: false }],
-    quantity: { type: Number, required: true },
-    categories: [{ type: String, required: true }],
-    subcategories: { type: String, required: true },
-    userid:{type:String,required:true},
-    nop:{type:Number,required:true}
+    quantity: { type: Number, required: false },
+    categories: [{ type: String, required: false }],
+    subcategories: { type: String, required: false },
+    userid:{type:String,required:false},
+    nop:{type:Number,required:false}
 
 })
 
